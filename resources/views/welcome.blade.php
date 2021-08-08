fa7de93f5a58ac38d34d0c1f8de1477cc65fc83c
Scott Sutton <scosut@hotmail.com>
2021-08-08 09:36:09 -0700

    Added custom favorite icon.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Contact Store</title>
-  <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">  
+	<link rel="icon" type="image/png" href="{{ asset('public/favicon.png') }}">
 </head>
 <body class="fadeIn">
   <div class="container">
